5c76c8f8c0889eeadc968c9fd28281011e020cd8
Shahista O Kinoo <oomeezakinoo@gmail.com>
2021-04-11 21:27:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,7 +4,7 @@ IF (age <=13 && >=19) // True  {
 console.log("You are a teenager! ");
 
   else //False
-  console.log("You are not a teenager");
+  console.log("You are not a teenager ");
 }
 function teenager(age) {
 
